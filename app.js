eaba82263ca2d1297784fec6475ff8f2f21a69f4
JeonSunil <jeonsunil1225@gmail.com>
2025-03-01 21:17:28 +0900

    파일의 이름을 몇가지 변경하고, 원래 layout모듈로 불러오던 백틱으로 제작된, html 폼을 사용하지 않고, 따로 html파일을 만들어서, post요청에 따라, data, update, delete.html 파일을 불러오도록 만들었다. 위의 활동을 통해 '*'(와일드 카드)의 존재를 알게 되었다.

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
 const express = require('express');
+const dataForm = require('./public/src/module/dataForm');
+const updateForm = require('./public/src/module/updateForm');
+const deleteForm = require('./public/src/module/deleteForm');
 const app = express();
 const port = 8000;
 
 app.use(express.static('public'));
+// 정적으로 public 폴더를 불러온다.
 app.use('/data', express.static('data'));
+// 정적으로 data 폴더를 불러온다.
 
 app.listen(port, function() {
 console.log( `http://localhost:${port}/ is Open..`)
+});
+// 서버 실행 코드
+
+app.post('/data', function(req,res) {
+  // post요청url이 /data일 때
+  dataForm(req,res);
+  // dataForm 모듈을 실행.
+  req.on('end', function() {
+  res.sendFile(__dirname + '/public/data.html');
+  // dataForm 모듈실행이 완료되면, 현재 app.js경로 /public폴더 안의 data.html을 불러온다.
+  });
+});
+
+app.post('/update*', function(req,res) {
+  // '*'는 와일드 카드로, 문자열 뒤에 어떠한 문자든 올 수 있음을 의미한다.
+  //  /update 자체를 포함한 /update로 시작하는 모든 문자열의 경로를 불러온다.
+  // 즉 일부분 startsWith의 역할을 할 수 있기에 나는 와일드 카드를 사용하여 startsWith의 역할을 대신 하였다.
+  // post요청의 url이 /update로 시작하는 문자열 일 때, 
+  const i = req.url.split('update')[1];
+  // 요청하는 url의 update뒤의 글자를 i로 지정.
+  updateForm(req,res,i);
+  // updateForm 모듈을 사용.
+  req.on('end', function() {
+  res.sendFile(__dirname + '/public/update.html');
+  // dataForm 모듈실행이 완료되면, 현재 app.js경로 /public폴더 안의 update.html을 불러온다.
+
+  });
+});
+
+app.post('/delete*', function(req,res) {
+  // '*'는 와일드 카드로, 문자열 뒤에 어떠한 문자든 올 수 있음을 의미한다. 
+  // /delete 자체를 포함한 /delete로 시작하는 모든 문자열의 경로를 불러온다.
+  // 즉 일부분 startsWith의 역할을 할 수 있기에 나는 와일드 카드를 사용하여 startsWith의 역할을 대신 하였다.
+  // post요청의 url이 /delete로 시작하는 문자열 일 때, 
+  const i = req.url.split('delete')[1];
+  // 요청하는 url의 delete뒤의 글자를 i로 지정.
+  deleteForm(req,res,i);
+  // deleteForm 모듈을 사용.
+  req.on('end', function() {
+  res.sendFile(__dirname + '/public/delete.html');
+  // dataForm 모듈실행이 완료되면, 현재 app.js경로 /public폴더 안의 delete.html을 불러온다.
+
+  });
 });
