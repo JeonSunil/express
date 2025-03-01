04eee8675c3f7e2900679474e9f7dcf252a66393
JeonSunil <jeonsunil1225@gmail.com>
2025-03-01 20:11:05 +0900

    json데이터를 불러오지 못하는 듯 하여, 따로, data.json파일이 따로 data폴더에 들어있어서 라고 판단. data폴더도 static으로 불러오도록 만들었다. 결과 - 목록 구현이 정상적으로 이루어짐. 이제 post처리만 해주면 될 듯

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8000;
 
 app.use(express.static('public'));
+app.use('/data', express.static('data'));
 
 app.listen(port, function() {
 console.log( `http://localhost:${port}/ is Open..`)
